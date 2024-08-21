b209c776edc961c112f4cd215332ee504f256ee0
vega <devid367552@gmail.com>
2024-08-21 20:02:57 +0300

    add Welcome Header component on main page with chart and balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,28 @@
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
+
 const Home = () => {
+    const loggedIn = {firstName: 'David'}
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Guest"}
+                        subtext="Access and manage your account and transactions"
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
