f0b09b7b615c9f6a0b95b0da95289f98310538fc
vega <devid367552@gmail.com>
2024-08-23 13:21:35 +0300

    complete sidebars layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,38 +1,39 @@
-import HeaderBox from "@/components/HeaderBox"
-import RightSidebar from "@/components/RightSidebar"
-import TotalBalanceBox from "@/components/TotalBalanceBox"
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-    const loggedIn = {firstName: 'David'}
+  const loggedIn = {
+    firstName: "David",
+    lastName: "Chernykh",
+    email: "example@mail.com",
+  };
 
-    return (
-        <section className="home">
-            <div className="home-content">
-                <header className="home-header">
-                    <HeaderBox 
-                        type="greeting"
-                        title="Welcome"
-                        user={loggedIn?.firstName || "Guest"}
-                        subtext="Access and manage your account and transactions"
-                    />
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions"
+          />
 
-                    <TotalBalanceBox
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1250.35}
-                    />
-                </header>
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
 
-                RECENT TRANSACTIONS
-            </div>
+      <RightSidebar user={loggedIn}
+       transactions={[]} 
+       banks={[{currentBalance:123.50}, {currentBalance:143.30}]} />
+    </section>
+  );
+};
 
-            <RightSidebar 
-            user={loggedIn}
-            transactions={[]}
-            banks={[]}
-            />
-        </section>
-    )
-}
-
-export default Home
+export default Home;
